66955ba903ae1fc26c0be4906484cae82f8f5bf8
JaburMacbookPro <jaburmail@gmail.com>
2018-09-09 19:42:34 -0300

    estilizacao do componente Item

--- src/pages/favorites/components/FavoriteItem/index.js
+++ src/pages/favorites/components/FavoriteItem/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import PropTypes from 'prop-types';
 
-const FavoriteItem = ({ favorite }) => (
-  <View>
-    <Text>{favorite.name}</Text>
+import styles from './styles';
+
+const FavoriteItem = ({ favorite: { name, owner, description } }) => (
+  <View style={styles.container} >
+    <Image style={styles.avatar} source={{ uri: owner.avatar_url }} />
+    <View style={styles.info}>
+      <Text style={styles.title}>{name}</Text>
+      <Text numberOfLines={2} style={styles.description}>{description}</Text>
+    </View>
   </View>
 );
+
+FavoriteItem.propType = {
+  favorite: PropTypes.shape({
+    name: PropTypes.string,
+    description: PropTypes.string,
+    owner: PropTypes.shape({
+      avatar_url: PropTypes.string,
+    }),
+  }).isRequired,
+}
 
 export default FavoriteItem;

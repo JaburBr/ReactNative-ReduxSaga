df1df8bc0f94e94a42c61422435fb34c53f2d36c
JaburMacbookPro <jaburmail@gmail.com>
2018-09-09 22:32:01 -0300

    Configuracao plugin do Saga no Reactotron

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,5 +1,6 @@
 import Reactotron, { asyncStorage } from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
+import sagaPlugin from 'reactotron-redux-saga';
 
 if (__DEV__) {
 
@@ -7,6 +8,7 @@ if (__DEV__) {
     .configure() //.configure({ host: '192.168.1.1' }) // controls connection & communication settings
     .useReactNative() // add all built-in react native plugins
     .use(reactotronRedux())
+    .use(sagaPlugin())
     .connect() // let's connect!
 
   console.tron = tron;
